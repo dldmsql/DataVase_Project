2cd5ff538babb62eed8d6832e23a391bb47f0c03
kyuri <kimkyuri90@gmail.com>
2020-07-06 15:05:02 +0900

    DVase 앱 카메라 기능

--- android/DVase_v1/app/src/main/java/com/dvase/dvase_v1/MainActivity.java
+++ android/DVase_v1/app/src/main/java/com/dvase/dvase_v1/MainActivity.java
@@ -1,13 +1,152 @@
 package com.dvase.dvase_v1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.provider.Settings;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int MY_PERMISSIONS_REQUEST_CAMERA = 1111;
+    private final int CAMERA_REQUEST_CODE = 1;
+
+    Button btnCamera;
+    ImageView img;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        checkPermission();
+
+        setup();
+    }
+
+    public void setup(){
+        btnCamera = (Button)findViewById(R.id.btnCamera);
+        img = (ImageView)findViewById(R.id.img);
+
+        btnCamera.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                if(IsCameraAvailable()){
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                    startActivityForResult(intent, CAMERA_REQUEST_CODE);
+                }
+            }
+        });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode==CAMERA_REQUEST_CODE){
+
+            Bundle bundle = data.getExtras();
+
+            Bitmap bitmap = (Bitmap) bundle.get("data");
+            img.setImageBitmap(bitmap);
+
+            String destFolder = Environment.getExternalStorageDirectory().toString();
+            String date = new SimpleDateFormat("yyyy_MM_dd_hh_mm_ss").format(new Date());
+
+            try {
+                FileOutputStream out = new FileOutputStream(destFolder + "/video/" + date + ".jpg");
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out);
+                Toast.makeText(this, "사진 자동 업로드", Toast.LENGTH_SHORT).show();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                Toast.makeText(this, "폴더를 찾지 못해 업로드하지 못했습니다.", Toast.LENGTH_SHORT).show();
+                return;
+            } catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(this, "이미지를 불러오지 못해 업로드하지 못했습니다.", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
+    }
+
+    //카메라 유무
+    public boolean IsCameraAvailable(){
+        PackageManager packageManager = getPackageManager();
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        List<ResolveInfo> list = packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+        return list.size() > 0;
+    }
+
+    public void checkPermission(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED ) {
+
+            if ((ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) ||
+                    (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA))) {
+                new AlertDialog.Builder(this)
+                        .setTitle("알림")
+                        .setMessage("저장소 권한이 거부되었습니다. 사용을 원하시면 설정에서 해당 권한을 직접 허용하셔야 합니다.")
+                        .setNeutralButton("설정", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                intent.setData(Uri.parse("package:" + getPackageName()));
+                                startActivity(intent);
+                            }
+                        })
+                        .setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                finish();
+                            }
+                        })
+                        .create()
+                        .show();
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_CAMERA);
+                Toast.makeText(this,"권한 승인이 필요합니다.",Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_CAMERA:
+                for (int i = 0; i < grantResults.length; i++) {
+                    // grantResults[] : 허용된 권한은 0, 거부한 권한은 -1
+                    if (grantResults[i] < 0) {
+                        Toast.makeText(MainActivity.this, "해당 권한을 활성화 하셔야 합니다.", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    else{
+                        Toast.makeText( MainActivity.this, "권한이 승인되었습니다.", Toast.LENGTH_SHORT ).show();
+                    }
+                }
+                break;
+        }
     }
 }
