77990b249655451ff2a8647a90312de4906b466c
lsj00416 <lsj00416@gmail.com>
2020-08-28 16:00:42 +0900

    웹뷰 정보 확인 페이지 제작

--- server/DVase/application/views/dvase/displayInformation.php
+++ server/DVase/application/views/dvase/displayInformation.php
@@ -4,6 +4,16 @@
     <head>
         <title> 식물 정보 </title>
 
+        <style>
+            .head-title{
+                font-weight: bold;
+                font-size: 0.8em;
+            }
+            .table-context{
+                font-size: 0.8em;
+            }
+        </style>
+
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
@@ -14,7 +24,7 @@
     <body>
     <table class="table" style="width: 80%; text-align: center;" align="center">
             <tr>
-                <td colspan="6">
+                <td colspan="3">
                 <?
                 $dir = "./dvaseFolder/learnSetImage/".$plant["eng_name"]."/";
                 $url = "http://15.164.251.97/dvaseFolder/learnSetImage/".$plant["eng_name"]."/";
@@ -29,37 +39,46 @@
                 </td>
             </tr>
             <tr>
-                <td>이름</th>
-                <td colspan="5"> <?= $plant["name"] ?> </td>
+                <td class="head-title" >이름</th>
+                <td class="table-context" colspan="2"> <?= $plant["name"] ?> </td>
             </tr>
             <tr>
-                <td>영문이름</th>
-                <td colspan="5"> <?= $plant["eng_name"] ?> </td>
+                <td class="head-title">영문명</th>
+                <td class="table-context" colspan="3"> <?= $plant["eng_name"] ?> </td>
             </tr>
             <tr>
-                <td>꽃말</th>
-                <td colspan="5"> <?= $plant["flower"] ?> </td>
+                <td class="head-title">꽃말</th>
+                <td class="table-context" colspan="3"> <?= $plant["flower"] ?> </td>
             </tr>
             <tr>
-                <td>관리 수준</th>
-                <td colspan="5"> <?= $plant["care"] ?> </td>
+                <td class="head-title">관리</th>
+                <td class="table-context" colspan="3">
+                    <?
+                        $level = $plant["care"];
+                        if( $level == "easy" ){
+                            echo "쉬운 편";
+                        } else if( $level == "normal" ){
+                            echo "보통";
+                        } else{
+                            echo "까다로움";
+                        }
+                    ?>
+                </td>
             </tr>
             <tr>
-                <td>물 주기</th>
-                <td colspan="5"> <?= $plant["water"] ?> </td>
+                <td class="head-title">물</th>
+                <td class="table-context" colspan="4"> <?= $plant["water"] ?> </td>
             </tr>
             <?
             $count = 1;
-            echo "<script> console.log(".sizeof( $plant["features"] ).") </script>";
             for( $i = 0; $i < sizeof( $plant["features"] ); $i++ ){
-                echo "<script> console.log(".$i.") </script>";
                 echo '
                 <tr>
-                    <td> 특징 '.$count.' </td>
-                    <td colspan="5"> '.$plant["features"][$i]["feature"].'</td>
+                    <td class="head-title"> 특징 '.$count.' </td>
+                    <td class="table-context" colspan="4"> '.$plant["features"][$i]["feature"].'</td>
                 </tr>
                 ';
-                $i++;
+                $count++;
             }
 
             ?>
